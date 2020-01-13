b6c4f747edd2add7ecca760e73149ccdd70bf25e
ahtamimi <ah.tamimi@gmail.com>
2020-01-13 17:10:34 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-mocha.setup('bdd');
+
 
 describe('sum', function () {
     it('should return sum of arguments', function () {
@@ -20,11 +20,29 @@ describe('sum2', function () {
       });
   });
 
-  mocha.setup('bdd');
-  let res = mocha.run();
-  script= document.createElement("div"); 
-  script.id = "run-res";
-  script.appendChild(document.createTextNode(Flatted.stringify(res)));
-  document.body.appendChild(script);
-  console.log('-----' ,res);
-//   localStorage.setItem('res', Flatted.stringify(res));
+  let res = {passed: [], failed: [], pending: [], success: true};
+  mocha.reporter('min');
+  mocha.run()
+  .on(EVENT_TEST_END, function(test) {
+      res.file = test.file;
+  })
+  .on(EVENT_TEST_PASS, function(test) {
+      res.passed.push(getTestData(test));
+  })
+  .on(EVENT_TEST_PENDING, function(test) {
+      res.pending.push(getTestData(test));
+  })
+  .on(EVENT_TEST_FAIL, function(test, err) {
+      res.failed.push(getTestData(test, err));
+  })
+  .on(EVENT_RUN_END, function() {
+      if(res.failed.length > 0)
+          res.success = false;    
+      
+      script= document.createElement("div"); 
+      script.id = "run-res";
+      script.appendChild(document.createTextNode(Flatted.stringify(res)));
+      document.body.appendChild(script);
+      
+      return resolve(res);
+  });
