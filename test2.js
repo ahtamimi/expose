0111ecbb17f43719e585c1be8507be9a59318bf5
ahtamimi <ah.tamimi@gmail.com>
2020-01-14 16:40:53 +0200

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,20 @@
+mocha.setup('bdd');
+
+describe('sum', function () {
+    it('should return sum of arguments', function () {
+      chai.expect(1 + 2).to.equal(3);
+    });
+    it('should return sum of arguments', function () {
+        chai.expect( 2).to.equal(3);
+      });
+  });
+
+  
+describe('sum2', function () {
+    it('should return sum of arguments', function () {
+      chai.expect(1 + 2).to.equal(3);
+    });
+    it('should return sum of arguments', function () {
+        chai.expect( 2).to.equal(3);
+      });
+  });
